1a142d81576568ebd0c8ff0ddf2b0faa86a5d6ec
agata-sm <smialowskaa@gmail.com>
2022-04-19 23:52:21 +0200

    modify DUPCHECK in juicer_rackham.sh

--- scripts_modified/juicer_rackham.sh
+++ scripts_modified/juicer_rackham.sh
@@ -977,6 +977,9 @@ if [ -z $postproc ]
 #temp solution
 awkscript='BEGIN{sscriptname = sprintf("%s/.%s_rmsplit.slurm", debugdir, groupname);}NR==1{if (NF == 2 && $1 == $2 ){print "Sorted and dups/no dups files add up"; printf("#!/bin/bash -l\n#SBATCH -o %s/dup-rm.out\n#SBATCH -e %s/dup-rm.err\n#SBATCH -p %s\n#SBATCH -J %s_msplit0\n#SBATCH -d singleton\n#SBATCH -t 1440\n#SBATCH -c 1\n#SBATCH --ntasks=1\n#SBATCH -A g2021025\ndate;\nrm %s/*_msplit*_optdups.txt; rm %s/*_msplit*_dups.txt; rm %s/*_msplit*_merged_nodups.txt;rm %s/split*;\ndate\n", debugdir, debugdir, queue, groupname, dir, dir, dir, dir) > sscriptname; sysstring = sprintf("sbatch %s", sscriptname); system(sysstring);close(sscriptname); }else{print "Problem"; print "***! Error! The sorted file and dups/no dups files do not add up, or were empty."}}'
 
+# better solution
+awkscript='BEGIN{sscriptname = sprintf("%s/.%s_rmsplit.slurm", debugdir, groupname);}NR==1{if (NF == 2 && $1 == $2 ){print "Sorted and dups/no dups files add up"; printf("#!/bin/bash -l\n#SBATCH -o %s/dup-rm.out\n#SBATCH -e %s/dup-rm.err\n#SBATCH -p %s\n#SBATCH -J %s_msplit0\n#SBATCH -d singleton\n#SBATCH -t 1440\n#SBATCH -c 1\n#SBATCH --ntasks=1\n#SBATCH -A %s\ndate;\nrm %s/*_msplit*_optdups.txt; rm %s/*_msplit*_dups.txt; rm %s/*_msplit*_merged_nodups.txt;rm %s/split*;\ndate\n", debugdir, debugdir, queue, groupname, userstring, dir, dir, dir, dir) > sscriptname; sysstring = sprintf("sbatch %s", sscriptname); system(sysstring);close(sscriptname); }else{print "Problem"; print "***! Error! The sorted file and dups/no dups files do not add up, or were empty."}}' ###### change here PM
+
 
     jid=`sbatch <<- DUPCHECK | egrep -o -e "\b[0-9]+$"
 #!/bin/bash -l
@@ -994,7 +997,7 @@ $userstring
 	date
 	ls -l ${outputdir}/merged_sort.txt | awk '{printf("%s ", \\\$5)}' > $debugdir/dupcheck-${groupname}
 	ls -l ${outputdir}/merged_nodups.txt ${outputdir}/dups.txt ${outputdir}/opt_dups.txt | awk '{sum = sum + \\\$5}END{print sum}' >> $debugdir/dupcheck-${groupname}
-awk -v debugdir=$debugdir -v queue=$queue -v groupname=$groupname -v userstring=$userstring -v dir=$outputdir '$awkscript' $debugdir/dupcheck-${groupname}
+awk -v debugdir=$debugdir -v queue=$queue -v groupname=$groupname -v user=$user -v dir=$outputdir '$awkscript' $debugdir/dupcheck-${groupname}
         date  
 DUPCHECK`
     sbatch_wait="#SBATCH -d afterok:$jid"
